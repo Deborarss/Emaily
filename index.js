c3f143e48f9bdf1ff0a31519695c3a0058694744
Deborarss <debora_rss@hotmail.com>
2019-04-18 17:00:17 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Importing Express library
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+// Dynamic Port Binding (Heroku tell us which port our app will use, so we need to make sure we listen to the port they tell us to)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
