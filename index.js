ef4dd8444e58a24ab2e96557646e8ebd80a6cbb6
Deborarss <debora_rss@hotmail.com>
2019-04-19 14:51:50 -0300

    configuring oauth

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 // Importing Express library
 const express = require('express');
+// Importing Passport library
+const passport = require('passport');
+// Importing Passport Google oauth 2.0 library
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// Importing keys
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);  
+        console.log('refreshToken', refreshToken); 
+        console.log('profile', profile); 
+    })
+);
+
+// Route Handler
+/*app.get('/', (req, res) => {
     res.send({ hi: 'Débora' });
 });
+*/
+
+// Route Handler
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+// Route Handler
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Dynamic Port Binding (Heroku tell us which port our app will use, so we need to make sure we listen to the port they tell us to)
 const PORT = process.env.PORT || 5000;
