56b7ecf572102cedbeb78e816ae0191831130c29
Deborarss <debora_rss@hotmail.com>
2019-04-21 14:36:46 -0300

    testing authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,13 +3,19 @@ const passport = require('passport');
 
 // Exporting function
 module.exports = app => {
-    // Route Handler
-    app.get('/auth/google', passport.authenticate('google', {
-            scope: ['profile', 'email'] 
-        }
-    ));
+  // Route Handler
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
 
-    // Route Handler
-    app.get('/auth/google/callback', passport.authenticate('google'));
+  // Route Handler
+  app.get('/auth/google/callback', passport.authenticate('google'));
 
-}
+  // Route Handler
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
