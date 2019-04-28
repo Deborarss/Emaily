281cd4d4a3a27fb98d2752eb71c887697942c36f
Debora <debora_rss@hotmail.com>
2019-04-28 14:24:26 -0300

    finished auth flow

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ require('./routes/authRoutes')(app);
 
 // Dynamic Port Binding (Heroku tell us which port our app will use, so we need to make sure we listen to the port they tell us to)
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT); 
+
